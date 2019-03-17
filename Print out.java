4ed3e05feea12f6e88003205f509c8b78f6ca19c
Mahdi Heydari <mahdiheydarimajid@gmail.com>
2019-03-17 11:22:21 +0330

    Add files via upload

---
+++ Print out.java
@@ -0,0 +1,15 @@
+package oop;
+
+import java.util.Scanner;
+
+public class OOP {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println("\tJ\tA\tV\tV\tA");
+        System.out.println("\tJ      A A\t V     V       A A");
+        System.out.println("   J\tJ     AAAAA\t  V   V       AAAAA");
+        System.out.println("    J  J     A     A\t    V        A\t   A");
+    }
+}
